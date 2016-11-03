ede97e722ab8ae4891d2a847fa320bcdf8a8900b
Rutger Weemhoff <rutger@direct-control.nl>
2016-11-03 20:51:38 +0100

    Sort parameters in alphabetical order for payload. Fixes #1

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ var crypto = require('crypto'),
 	querystring = require('query-string')
 
 const 
-	length = 32,
-	iterations = 1,
-	urlbase = 'https://apps.ticketmatic.com/widgets/',
-	parameters = {
+	URLBASE = 'https://apps.ticketmatic.com/widgets/',
+	PARAMETERS = {
 		all: ["l", "contactid", "skinid", "orderid", "returnurl"],
 		addtickets: ["event", "product", "flow", "reservemoretickets", "saleschannelid", "ticketinfo", "extraevents", "extraproducts", "edit", "panels", "oncompletion", "withauthentication"],
 		basket: ["flow", "edit", "reservemoretickets", "panels", "oncompletion"],
 		checkout: ["panels", "oncompletion"]
 	},
-
+	
 	filterWithKeys = (pred, obj) => R.pipe(
   		R.toPairs,
   		R.filter(R.apply(pred)),
@@ -25,7 +23,7 @@ const
 
 	hasValue = R.pipe(R.isNil, R.not),
 
-	concatFields = R.pipe(R.filter(inPayload), R.filter(hasValue), R.toPairs, R.flatten, R.join('')),
+	concatFields = R.pipe(R.filter(inPayload), R.filter(hasValue), R.toPairs, R.sortBy(R.head), R.flatten, R.join('')),
 
 	generatePayload = (accesskey, accountname, properties) => R.join('', [accesskey, accountname, concatFields(properties)]),
 
@@ -34,7 +32,7 @@ const
 		return crypto.createHmac('sha256', secret).update(payload).digest('hex')
 	},
 
-	validProperty = R.curry((type, key, value) => R.and(R.contains(key, R.union(parameters.all, parameters[type])), hasValue(value)) )
+	validProperty = R.curry((type, key, value) => R.and(R.contains(key, R.union(PARAMETERS.all, PARAMETERS[type])), hasValue(value)) )
 
 
 module.exports = {
@@ -42,9 +40,9 @@ module.exports = {
 	generateUrl: (client, widgettype, properties) => {
 		let validproperties = filterWithKeys(validProperty(widgettype))(properties),
 			signature = generateSignature(client.key, client.shortname, validproperties, client.secret),
-        	parameters = R.merge(validproperties, {accesskey: client.key, signature: signature}),
-        	query = querystring.stringify(parameters)
+        	_parameters = R.merge(validproperties, {accesskey: client.key, signature: signature}),
+        	query = querystring.stringify(_parameters)
 
-		return R.join('', [urlbase, client.shortname, '/', widgettype, '?', query])		
+		return R.join('', [URLBASE, client.shortname, '/', widgettype, '?', query])		
 	}
 }
